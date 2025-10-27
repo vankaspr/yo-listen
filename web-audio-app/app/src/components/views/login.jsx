0c81206f897f7128ad77b14aad7358ea8cc7d440
vankaspr <msvankaspr@gmail.com>
2025-10-27 15:20:01 +0500

    🫰 Create lofin ando register form

---
+++ web-audio-app/app/src/components/views/login.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { ConfirmLogin } from '../buttons/confirming';
+import { GithubButton } from '../buttons/social';
+import '../../style/forms.css';
+
+export function LoginForm() {
+  const [formData, setFormData] = useState({
+    login: '',
+    password: '',
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handlSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handlSubmit}>
+            <label>Email or username:</label>
+            <input 
+            type="text"
+            placeholder='Enter your email or username'
+            name="login"
+            value={formData.login}
+            onChange={handleChange}
+            />
+
+            <label>Password:</label>
+            <input 
+            type="password"
+            placeholder='Enter your password'
+            name='password'
+            value={formData.password}
+            onChange={handleChange}
+            />
+        </form>
+        <ConfirmLogin formData={formData} />
+
+        <div>
+            <h3>Or login with other social:</h3>
+            <GithubButton />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ web-audio-app/app/src/components/views/register.jsx
@@ -0,0 +1,78 @@
+import { ConfirmRegistration } from '../buttons/confirming';
+import { GithubButton } from '../buttons/social';
+import '../../style/forms.css';
+import { useState } from 'react';
+
+export function RegisterForm() {
+  const [formData, setFormData] = useState({
+    email: '',
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handlSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handlSubmit}>
+          <label>Email: </label>
+          <input
+            type="text"
+            placeholder="Enter your email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+
+          <label>Username: </label>
+          <input
+            type="text"
+            placeholder="Enter your username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+
+          <label>Password: </label>
+          <input
+            type="password"
+            placeholder="Create password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+
+          <label>Repeat password: </label>
+          <input
+            type="password"
+            placeholder="Repeat password"
+            name="passwordConfirm"
+            value={formData.passwordConfirm}
+            onChange={handleChange}
+          />
+
+          <h5>I have read the user agreement</h5>
+
+        </form>
+        <ConfirmRegistration formData={formData} />
+
+        <div>
+          <h3>Or register with other social:</h3>
+          <GithubButton />
+        </div>
+      </div>
+    </>
+  );
+}
