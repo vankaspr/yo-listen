a2811991bf328b4e256d2979da6475e2c61f2a45
vankaspr <msvankaspr@gmail.com>
2025-10-27 15:18:16 +0500

    🥴 Create const api path

---
+++ web-audio-app/app/src/api/path.ts
@@ -0,0 +1,24 @@
+export const API_BASE_URL = 'http://localhost:8000/api';
+
+export const API_ENDPOINTS = {
+    AUTH: {
+        REGISTER: `${API_BASE_URL}/auth/register`,
+        LOGIN: `${API_BASE_URL}/auth/login`,
+        VERIFY: `${API_BASE_URL}/auth/verify-email`,
+        GITHUB:  `${API_BASE_URL}/auth/github`,
+        GITHUB_CALLBACK: `${API_BASE_URL}/auth/github/callback`,
+        FORGOT_PASSWORD: `${API_BASE_URL}/auth/forgot-password`,
+        RESET_PASSWORD: `${API_BASE_URL}/auth/reset-password`,
+        REFRESH: `${API_BASE_URL}/auth/refresh`,
+    },
+
+    USER: {
+        ME: `${API_BASE_URL}/user/me`,
+    },
+
+    ADMIN: {
+        STATISTIC: `${API_BASE_URL}/admin/statistic/users`,
+        DEACTIVATE: `${API_BASE_URL}/admin/deactivate/{user_id}`,
+        REACTIVATE: `${API_BASE_URL}/admin/reactivate/{user_id}`,
+    }
+} as const;
