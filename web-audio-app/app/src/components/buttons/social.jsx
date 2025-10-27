1247673ea65d1d8f3d61f3ce6cb02f4f74ebe3f1
vankaspr <msvankaspr@gmail.com>
2025-10-27 15:20:43 +0500

    💋 Create another button: github

---
+++ web-audio-app/app/src/components/buttons/social.jsx
@@ -0,0 +1,14 @@
+export function GithubButton() {
+
+    const handleClick = () => {
+        alert("Github clicked!")
+    }
+    
+    return (
+        <button 
+        onClick={handleClick}
+        >
+        Github
+        </button>
+    )
+}
