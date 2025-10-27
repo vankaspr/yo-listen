d02eafa7cfc2d602f08c4c7299d7b75effcc8858
vankaspr <msvankaspr@gmail.com>
2025-10-27 16:13:06 +0500

    🎀 Update App: add active form

--- web-audio-app/app/src/App.jsx
+++ web-audio-app/app/src/App.jsx
@@ -1,15 +1,26 @@
-import { RegisterButton } from './components/buttons/register'
-import { LoginButton } from './components/buttons/login';
+import { useState } from 'react';
+import { RegisterForm } from './components/views/register';
+import { LoginForm } from './components/views/login';
+import { ForgotPasswordForm } from './components/views/forgot_password';
 
 function App() {
+  const [activeForm, setActiveForm] = useState(null);
 
   return (
     <>
       <div>Welcome to my app 👹</div>
 
       <div>
-        <RegisterButton />
-        <LoginButton />
+        {!activeForm && (
+          <div>
+            <button onClick={() => setActiveForm('register')}>Register</button>
+            <button onClick={() => setActiveForm('login')}>Login</button>
+          </div>
+        )}
+
+        {activeForm === 'register' && <RegisterForm setActiveForm={setActiveForm}/>}
+        {activeForm === 'login' && <LoginForm setActiveForm={setActiveForm}/>}
+        {activeForm === 'forgot-password' && <ForgotPasswordForm setActiveForm={setActiveForm}/>}
       </div>
     </>
   );
