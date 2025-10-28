e2ae09f5b75027d5fe7d22ff6114274ec7e403c0
vankaspr <msvankaspr@gmail.com>
2025-10-28 15:55:56 +0500

    🐫 Update github button

--- web-audio-app/app/src/components/buttons/social.jsx
+++ web-audio-app/app/src/components/buttons/social.jsx
@@ -1,7 +1,13 @@
+import { API_ENDPOINTS } from "../../api/path";
+
 export function GithubButton() {
-  const handleClick = () => {
-    alert('Github clicked!');
+  const handleClick = async () => {
+    try {
+      window.location.href = API_ENDPOINTS.AUTH.GITHUB;
+    } catch (error) {
+      console.log('Error github auth: ', error);
+    }
   };
 
-  return <button onClick={handleClick}>Github</button>;
+  return <button onClick={handleClick}>Login with GitHub</button>;
 }
