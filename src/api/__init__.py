2208618044656ad7a0e853883d05a124e8a43a17
vankaspr <msvankaspr@gmail.com>
2025-11-04 13:45:11 +0500

    🦦 Include new routers to main

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,6 +3,9 @@ from core.config import settings
 from .auth import router as auth_router
 from .user import router as user_router
 from .admin import router as admin_router
+from .post import router as post_router
+from .like import router as like_router
+from .comment import router as comment_router
 
 router = APIRouter(
     prefix=settings.api.prefix
@@ -10,4 +13,9 @@ router = APIRouter(
 
 router.include_router(auth_router)
 router.include_router(user_router)
+
+router.include_router(post_router)
+router.include_router(like_router)
+router.include_router(comment_router)
+
 router.include_router(admin_router)
