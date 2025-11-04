8596ffcfd5413f68a82ab9a9e018ef8829ce681e
vankaspr <msvankaspr@gmail.com>
2025-11-04 17:05:31 +0500

    🤯💀 Update post router

--- src/api/post.py
+++ src/api/post.py
@@ -3,6 +3,9 @@ from typing import Annotated
 from core.database.models import User
 from core.dependency.user import get_current_user
 from core.config import settings
+from core.database.schemas.post import PostResponse, PostCreate, PostUpdate
+from core.dependency.services import get_post_like_comment_service
+from core.services.PLC import PostLikeCommentService
 
 router = APIRouter(
     prefix=settings.api.post,
@@ -10,14 +13,23 @@ router = APIRouter(
 )
 
 
-@router.post("/create")
+@router.post("/create", response_model=PostResponse)
 async def create_post(
+    post_data: PostCreate,
     user: Annotated[
         User,
         Depends(get_current_user),
     ],
+    service: Annotated[PostLikeCommentService, Depends(get_post_like_comment_service)],
 ):
-    pass
+    post = await service.create_post(
+        user_id=user.id,
+        title=post_data.title,
+        content=post_data.content,
+        tag=post_data.tag,
+    )
+
+    return post
 
 
 @router.get("/")
@@ -26,8 +38,12 @@ async def get_posts(
         User,
         Depends(get_current_user),
     ],
+    service: Annotated[
+        PostLikeCommentService,
+        Depends(get_post_like_comment_service),
+    ],
 ):
-    pass
+    return await service.get_all_user_posts(user_id=user.id)
 
 
 @router.get("/{post_id}")
@@ -37,19 +53,35 @@ async def get_post(
         User,
         Depends(get_current_user),
     ],
+    service: Annotated[
+        PostLikeCommentService,
+        Depends(get_post_like_comment_service),
+    ],
 ):
-    pass
+    return await service.get_post_by_id(post_id=post_id)
 
 
 @router.patch("/{post_id}")
 async def update_post(
     post_id: int,
+    update_data: PostUpdate,
     user: Annotated[
         User,
         Depends(get_current_user),
     ],
+    service: Annotated[
+        PostLikeCommentService,
+        Depends(get_post_like_comment_service),
+    ],
 ):
-    pass
+
+    update_dict = update_data.model_dump(exclude_unset=True)
+
+    return await service.update_post(
+        post_id=post_id,
+        user_id=user.id,
+        **update_dict,
+    )
 
 
 @router.delete("/{post_id}")
@@ -59,5 +91,9 @@ async def delete_post(
         User,
         Depends(get_current_user),
     ],
+    service: Annotated[
+        PostLikeCommentService,
+        Depends(get_post_like_comment_service),
+    ],
 ):
-    pass
+    return await service.delete_post(user_id=user.id, post_id=post_id)
