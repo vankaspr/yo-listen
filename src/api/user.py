e8d7cc0e559d46f11c4df440d0f2fa1941920107
vankaspr <msvankaspr@gmail.com>
2025-11-05 13:14:14 +0500

    💫 Cleaning code

--- src/api/user.py
+++ src/api/user.py
@@ -7,30 +7,23 @@ from core.dependency.services import get_profile_service
 from core.config import settings
 from core.database.schemas.profile import BioUpdate, AvatarUpdate
 
-router = APIRouter(
-    prefix=settings.api.user,
-    tags=["User"]
-)
+router = APIRouter(prefix=settings.api.user, tags=["User"])
+
 
 @router.get("/me")
-async def me(
-    user: Annotated[
-        User,
-        Depends(get_current_user)
-    ]
-):
+async def me(user: Annotated[User, Depends(get_current_user)]):
     return {
         "message": "👺 - Current user info:",
         "id": user.id,
-        "email": user.email, 
+        "email": user.email,
         "username": user.username,
         "is_active": user.is_active,
         "is_verified": user.is_verified,
         "is_superuser": user.is_superuser,
     }
-    
 
-@router.get('/me/profile')
+
+@router.get("/me/profile")
 async def profile(
     user: Annotated[
         User,
@@ -39,11 +32,11 @@ async def profile(
     profile_service: Annotated[
         ProfileService,
         Depends(get_profile_service),
-    ]
+    ],
 ):
-    #profile = await profile_service.get_or_create_profile(user=user)
+    # profile = await profile_service.get_or_create_profile(user=user)
     profile = await profile_service.get_user_profile(user.id)
-    
+
     response_data = {
         "id": user.id,
         "email": user.email,
@@ -51,26 +44,19 @@ async def profile(
         "is_verified": user.is_verified,
         "created_at": user.created_at,
     }
-    
+
     if profile:
-        response_data.update(
-            {
-                "avatar": profile.avatar,
-                "bio": profile.bio,
-                "theme": profile.theme,
-            }
-        )
+        response_data |= {
+            "avatar": profile.avatar,
+            "bio": profile.bio,
+            "theme": profile.theme,
+        }
+
         message = "Here's full profile of user:"
     else:
         message = "Profile not found"
-    
-    
-    return {
-        "message": message,
-        **response_data
-        
-    }
-    
+
+    return {"message": message, **response_data}
 
 
 @router.patch("/me/profile/bio")
@@ -85,7 +71,7 @@ async def update_bio(
         Depends(get_profile_service),
     ],
 ):
-    
+
     return await profile_service.update_bio(user.id, update.bio)
 
 
@@ -101,7 +87,5 @@ async def update_avatar(
         Depends(get_profile_service),
     ],
 ):
-    
+
     return await profile_service.update_avatar(user.id, update.avatar)
-
-
