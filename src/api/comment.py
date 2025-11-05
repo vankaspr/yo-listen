74ef72e89fb9fcd1db9995281fc97501fde9f51c
vankaspr <msvankaspr@gmail.com>
2025-11-05 14:18:51 +0500

    👹 Update comment router

--- src/api/comment.py
+++ src/api/comment.py
@@ -3,6 +3,10 @@ from typing import Annotated
 from core.database.models import User
 from core.dependency.user import get_current_user
 from core.config import settings
+from core.database.schemas.comment import CommentCreate, CommentUpdate
+from core.dependency.services import get_post_like_comment_service
+from core.services.PLC import PostLikeCommentService
+
 
 router = APIRouter(
     prefix=settings.api.comment,
@@ -13,12 +17,19 @@ router = APIRouter(
 @router.post("/post/{post_id}")
 async def create_comment(
     post_id: int,
+    data: CommentCreate,
     user: Annotated[
         User,
         Depends(get_current_user),
     ],
+    service: Annotated[
+        PostLikeCommentService,
+        Depends(get_post_like_comment_service),
+    ],
 ):
-    pass
+    return await service.create_comment(
+        user_id=user.id, post_id=post_id, content=data.content
+    )
 
 
 @router.delete("/{comment_id}")
@@ -28,19 +39,32 @@ async def delete_comment(
         User,
         Depends(get_current_user),
     ],
+    service: Annotated[
+        PostLikeCommentService,
+        Depends(get_post_like_comment_service),
+    ],
 ):
-    pass
+    return await service.delete_comment(comment_id=comment_id, user_id=user.id)
 
 
 @router.patch("/{comment_id}")
 async def update_comment(
     comment_id: int,
+    update_data: CommentUpdate,
     user: Annotated[
         User,
         Depends(get_current_user),
     ],
+    service: Annotated[
+        PostLikeCommentService,
+        Depends(get_post_like_comment_service),
+    ],
 ):
-    pass
+    return await service.update_comment(
+        comment_id=comment_id,
+        content=update_data.content,
+        user_id=user.id,
+    )
 
 
 @router.get("/post/{post_id}")
@@ -50,5 +74,9 @@ async def get_post_comments(
         User,
         Depends(get_current_user),
     ],
+    service: Annotated[
+        PostLikeCommentService,
+        Depends(get_post_like_comment_service),
+    ],
 ):
-    pass
+    return await service.get_post_comments(post_id=post_id)
