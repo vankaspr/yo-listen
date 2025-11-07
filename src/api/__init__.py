138c6ea13481aba167e5b5b3a170e4617410b5c7
vankaspr <msvankaspr@gmail.com>
2025-11-07 16:12:59 +0500

    💛 Create home router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -6,10 +6,13 @@ from .admin import router as admin_router
 from .post import router as post_router
 from .like import router as like_router
 from .comment import router as comment_router
+from .home import router as home_router
 
 router = APIRouter(
     prefix=settings.api.prefix
 )
+
+router.include_router(home_router)
 
 router.include_router(auth_router)
 router.include_router(user_router)
