aeffb6c0c5a4eccd1da58bc280d66510493ee110
vankaspr <msvankaspr@gmail.com>
2025-11-10 12:54:04 +0500

    ☂️ Update home router

--- src/api/home.py
+++ src/api/home.py
@@ -6,12 +6,11 @@ from core.services.PLC import PostLikeCommentService
 from core.services.user import UserService
 
 
-
-
 router = APIRouter(
     prefix=settings.api.home,
     tags=["Home"],
 )
+
 
 @router.get("/tranding-posts")
 async def tranding_posts(
@@ -27,6 +26,7 @@ async def tranding_posts(
         days=days,
     )
 
+
 @router.get("/tranding-users")
 async def tranding_users(
     limit: int,
@@ -38,28 +38,43 @@ async def tranding_users(
     return await service.get_tranding_users(limit=limit)
 
 
-
 @router.get("/stats")
 async def site_stats(
-    service: Annotated[
-        PostLikeCommentService,
-        Depends(get_post_like_comment_service)
-    ],
-    service_user: Annotated[
-        UserService,
-        Depends(get_user_service)
-    ]
+    service: Annotated[PostLikeCommentService, Depends(get_post_like_comment_service)],
+    service_user: Annotated[UserService, Depends(get_user_service)],
 ):
-    """ 
+    """
     Common stats about app
     """
     users = await service_user.get_all_users_count()
     posts = await service.get_all_posts_count()
     comments = await service.get_all_comments_count()
-    
+
     return {
         "message": "App stats",
         "users": users,
         "posts": posts,
         "comments": comments,
     }
+
+
+@router.get("/all-posts")
+async def all_posts(
+    limit: int,
+    service: Annotated[
+        PostLikeCommentService,
+        Depends(get_post_like_comment_service),
+    ],
+):
+    return await service.get_all_posts(limit=limit)
+
+
+@router.get("/tag-popular")
+async def popular_tags(
+    
+    service: Annotated[
+        PostLikeCommentService,
+        Depends(get_post_like_comment_service),
+    ],
+):
+    return await service.get_tranding_tag()
