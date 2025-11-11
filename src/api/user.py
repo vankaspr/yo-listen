68c9f39d1e1c174f77b88f1217ceedb9021628b9
vankaspr <msvankaspr@gmail.com>
2025-11-11 13:36:23 +0500

    👶 Update user router with SubscriptionService

--- src/api/user.py
+++ src/api/user.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter, Depends
 from typing import Annotated
 from core.database.models import User
-from core.services import ProfileService, PostLikeCommentService
+from core.services import (
+    ProfileService,
+    PostLikeCommentService,
+    SubscriptionService,
+)
 from core.dependency.user import get_current_user
-from core.dependency.services import get_profile_service, get_post_like_comment_service
+from core.dependency.services import (
+    get_profile_service,
+    get_post_like_comment_service,
+    get_subscription_service,
+)
 from core.config import settings
 from core.database.schemas.profile import BioUpdate, AvatarUpdate
 
@@ -102,3 +110,106 @@ async def user_liked_post(
     ],
 ):
     return await service.get_liked_post_by_user(user_id=user.id)
+
+
+# TODO: doit router for subscribers
+@router.post("/me/subscriptions/subscribe/{following_id}")
+async def subscribe(
+    following_id: int,
+    user: Annotated[
+        User,
+        Depends(get_current_user),
+    ],
+    service: Annotated[
+        SubscriptionService,
+        Depends(get_subscription_service),
+    ],
+):
+    """
+    Subscribe to another user
+
+    **following_id**: ID of the user you want to follow
+
+    Returns the created subscription object
+    """
+    return await service.create_subscription(
+        follower_id=user.id,
+        following_id=following_id,
+    )
+
+
+@router.get("/me/subscriptions/following")
+async def me_following(
+    user: Annotated[
+        User,
+        Depends(get_current_user),
+    ],
+    service: Annotated[
+        SubscriptionService,
+        Depends(get_subscription_service),
+    ],
+):
+    return await service.get_user_following(user_id=user.id)
+
+
+@router.get("/me/subscriptions/followers")
+async def my_followers(
+    user: Annotated[
+        User,
+        Depends(get_current_user),
+    ],
+    service: Annotated[
+        SubscriptionService,
+        Depends(get_subscription_service),
+    ],
+):
+    """
+    Get my followers
+    """
+    result = await service.get_user_followers(
+        user_id=user.id,
+    )
+
+    if not result:
+        return {
+            "message": "You don't have any subscribers yet.",
+            "count": 0,
+            "followers": result,
+        }
+
+    return {
+        "count": len(result),
+        "followers": result,
+    }
+
+
+@router.get("/{user_id}/follow-stats")
+async def follow_stats(
+    user: Annotated[
+        User,
+        Depends(get_current_user),
+    ],
+    service: Annotated[
+        SubscriptionService,
+        Depends(get_subscription_service),
+    ],
+):
+    return await service.get_subscriptions_stats(user_id=user.id)
+
+
+@router.delete("/me/subscriptions/unsubscribe/{following_id}")
+async def unsubscribe(
+    following_id: int,
+    user: Annotated[
+        User,
+        Depends(get_current_user),
+    ],
+    service: Annotated[
+        SubscriptionService,
+        Depends(get_subscription_service),
+    ],
+):
+    return await service.delete_subsription(
+        follower_id=user.id,
+        following_id=following_id,
+    )
