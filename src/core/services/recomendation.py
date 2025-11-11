8beeed1a8620bdd2b249ca51bc28e5b404453e79
vankaspr <msvankaspr@gmail.com>
2025-11-11 15:26:07 +0500

    🫸 Update rec service: add feed

--- src/core/services/recomendation.py
+++ src/core/services/recomendation.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import selectinload
 from core.database.models import (
     Post,
     Like,
+    Subscription,
 )
 from core.services.base import BaseService
 
@@ -16,16 +17,16 @@ logger = logging.getLogger(__name__)
 
 
 class RecommendationService(BaseService):
-    """ 
+    """
     Service for working with recommendations
     """
+
     def __init__(
         self,
         session: AsyncSession,
     ):
         super().__init__(session=session)
 
-    
     async def get_recommended_posts(
         self,
         user_id: int,
@@ -48,7 +49,7 @@ class RecommendationService(BaseService):
             user_tags_result = await self.session.execute(user_tags_stmt)
 
             if user_favorite_tags := user_tags_result.scalars().all():
-                # If there are any, then we find similar tags, 
+                # If there are any, then we find similar tags,
                 # excluding those posts that were written by the user himself.
                 stmt = (
                     select(Post)
@@ -76,9 +77,90 @@ class RecommendationService(BaseService):
 
             result = await self.session.execute(stmt)
             return result.scalars().all()
-            
-            
-        
+
         except SQLAlchemyError as e:
             logger.error("Error getting recommended posts: %s", e)
             raise error.DataBaseError("Database temporarily unavailable") from e
+
+    async def get_user_feed(
+        self,
+        user_id: int,
+        limit: int = 20,
+        skip: int = 0,
+    ) -> list[Post]:
+        """
+        Get personalized feed: posts from following + recommendations
+        """
+        try:
+            # get folowing posts feed
+            following_feed = await self.get_following_post(
+                user_id=user_id, skip=skip, limit=limit
+            )
+
+            # If there are few subscriptions, 
+            # we supplement the feed with recommendations
+            if len(following_feed) < limit:
+                remaining = limit - len(following_feed)
+                recommendation = await self.get_recommended_posts(
+                    user_id=user_id, limit=remaining
+                )
+                
+                feed = following_feed + recommendation
+                
+                #remove dublicate
+                return self._deduplicate_posts(feed)
+            
+            return following_feed
+
+        except SQLAlchemyError as e:
+            logger.error("Error getting recommended posts: %s", e)
+            raise error.DataBaseError("Database temporarily unavailable") from e
+
+    async def get_following_post(
+        self,
+        user_id: int,
+        skip: int = 0,
+        limit: int = 20,
+    ) -> list[Post]:
+        """
+        A feed of posts for a specific user based on who that user follows
+        """
+        try:
+            stmt = (
+                select(Post)
+                .join(Subscription, Post.user_id == Subscription.following_id)
+                .where(
+                    Subscription.follower_id == user_id,
+                    Post.is_published == True,
+                )
+                .order_by(desc(Post.created_at))
+                .offset(skip)
+                .limit(limit)
+                .options(
+                    selectinload(Post.author),
+                    selectinload(Post.comments),
+                    selectinload(Post.likes),
+                )
+            )
+
+            result = await self.session.execute(stmt)
+            return result.scalars().all()
+
+        except SQLAlchemyError as e:
+            logger.error("Error getting recommended posts: %s", e)
+            raise error.DataBaseError("Database temporarily unavailable") from e
+
+    def _deduplicate_posts(
+        self,
+        posts: list[Post],
+    ) -> list[Post]:
+        """ 
+        Remove duplicated posts from feed
+        """
+        seen = set()
+        unique_post = []
+        for post in posts:
+            if post.id not in seen:
+                seen.add(post.id)
+                unique_post.append(post)
+        return unique_post
